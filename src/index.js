fd5fd3c31517c1d6c1231144f898df60bc01a02c
Chipskein <abfn0905@gmail.com>
2022-06-30 14:48:30 -0300

    testando sockets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 const express=require('express');
 const app=express();
 const port=process.env.PORT || 8080;
+const http=require('http');
+const httpServer=http.createServer(app);
+const { Server }=require("socket.io");
+const io=new Server(httpServer);
+
 app.get('/',(req,res)=>{
-    return res.status(200).json('Running API');
+    return res.sendFile(__dirname+'/index.html')
 });
-app.listen(port,()=>console.log(`Started Server on ${port}`));
+io.on("connection",(socket)=>{
+    
+    console.log("Socket Connected:",socket.id)
+    
+    socket.on('disconnect', () => {
+        console.log('User Disconnected');
+    });
+    
+    socket.on('chat message', (msg) => {
+        console.log(`Message from client(${socket.id}) message: ` + msg);
+    });
+
+    io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
+
+});
+httpServer.listen(port,()=>console.log(`Started Server on ${port}`));
